cbad8ea36282c65f615db645bf99fa38b5798c57
maruthi0209 <sethumaruthi93@gmail.com>
2025-03-20 19:11:26 +0530

    added form input elements to body section

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,16 +4,17 @@ export const Body = function () {
             <div className="card m-4" id="body">
                 <div className="card-title text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. A explicabo, tempore iure obcaecati, aut doloribus, minima quisquam culpa itaque perferendis fuga porro. Error adipisci qui provident nobis consectetur. Nulla, velit.</div>
   <div className="card-body">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis culpa molestias earum possimus excepturi odit? Mollitia, cum obcaecati, iste officia libero labore ipsa, quos rerum soluta quas nam cumque dolorem?
-    Praesentium, et magni! Expedita maiores quidem enim accusamus magni rem fugiat iure harum quo aperiam, molestias dolore, dolor odio doloribus optio vitae at deserunt laborum voluptatibus distinctio, nisi modi veritatis.
-    Voluptatibus nesciunt nihil nisi temporibus distinctio id alias ullam vel, ad nulla assumenda natus at aperiam, debitis fugit cum? Eos tempore doloribus fugit neque porro labore excepturi eligendi officiis hic.
-    Et aspernatur sunt obcaecati eius molestias debitis iste alias quisquam nam assumenda saepe, ex delectus dolore eligendi esse optio reiciendis deserunt? Deserunt pariatur inventore at distinctio adipisci, placeat assumenda odio.
-    Doloribus ad est reprehenderit inventore, libero exercitationem labore ea suscipit quae maiores sunt ullam! Quibusdam, commodi? Odit, consequuntur reprehenderit numquam iure tempore rem laboriosam hic exercitationem neque amet minus eius!
-    Expedita, doloribus id. Blanditiis nesciunt facilis eaque dignissimos. Voluptatibus quis minus ipsum! Magnam, ad eveniet nihil ex, minima dolor architecto libero hic eaque sequi iusto! Vel ipsa quis officiis deleniti.
-    Et magnam, eveniet nesciunt ducimus vel omnis dignissimos ratione explicabo dolore dicta voluptas officiis, atque enim possimus recusandae incidunt dolores. Ex assumenda quae qui dolor quisquam quod sint dicta. Quae?
-    Suscipit, dolor natus accusamus magnam exercitationem corrupti voluptatem libero nesciunt, aperiam cumque assumenda aliquid aspernatur excepturi! Perferendis alias ullam repellendus praesentium! Officiis corporis culpa rerum, sit iure quaerat nam ad.
-    Eveniet culpa incidunt, error reiciendis blanditiis iusto maxime perspiciatis excepturi fugit, pariatur sint quae nemo quod est quisquam voluptates mollitia assumenda. Officia quam maiores unde blanditiis esse vero libero quo.
-    Doloremque ratione quae, laborum error quas eos omnis placeat, nam ex rem qui obcaecati harum aperiam necessitatibus, eum cumque facere. Maiores repellat, molestiae esse expedita quibusdam aliquid quaerat et blanditiis?
+  <div className="input-group mb-3">
+  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Enter your username" />
+  </div>
+
+  <div className="input-group mb-3">
+  <input type="password" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Enter your password" />
+  </div>
+
+  <button className="btn btn-outline-primary" type="button" id="button-addon1">Click Me!</button>
+
+
   </div>
 </div>
         </>
